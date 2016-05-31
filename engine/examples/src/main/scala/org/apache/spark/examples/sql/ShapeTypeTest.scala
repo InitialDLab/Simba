bbd50b514040f3c76fcbb39cc92e8201d2b1a646
gfl94 <gefeili.2013@gmail.com>
2016-05-31 14:06:22 +0800

    Delete redundant file for Shapefile format parser.

--- engine/examples/src/main/scala/org/apache/spark/examples/sql/ShapeTypeTest.scala
+++ engine/examples/src/main/scala/org/apache/spark/examples/sql/ShapeTypeTest.scala
@@ -43,23 +43,25 @@ object ShapeTypeTest {
       new StructField("shape", ShapeType, nullable = true)))
 
     // Polygon test
-    val shapefilePath = "/home/gefei/Downloads/gis/tl_2015_04_elsd/tl_2015_04_elsd.shp"
+    val shapefilePath = "/path/to/polygon-file"
     val gf = new GeometryFactory()
     val rdd = sc.parallelize(shapefile.Parser(shapefilePath)(gf)).map(r => (r.id, r.g))
     val df = rdd.toDF().show(5)    // toDF
 
-    val df2 = sqlContext.createDataFrame(rdd.map(org.apache.spark.sql.Row.fromTuple), schema).show(5)
-
+    val df2 = sqlContext.createDataFrame(rdd.map(org.apache.spark.sql.Row.fromTuple),
+      schema).show(5)
     rdd.toDF.registerTempTable("polygons")
-    sqlContext.sql("SELECT * FROM polygons").show(5) //sql test
+    sqlContext.sql("SELECT * FROM polygons").show(5) // sql test
 
 
     val rdd2 = sc.parallelize(1 to 10 map(i => Record(i, Point(Array(i, i, i)))))
     rdd2.toDF.show(5) // implicit convert
-    sqlContext.createDataFrame(rdd2.map(org.apache.spark.sql.Row.fromTuple), schema).show(5) //explicit covert
+    sqlContext.createDataFrame(rdd2.map(org.apache.spark.sql.Row.fromTuple),
+      schema).show(5) // explicit covert
     rdd2.toDF.registerTempTable("points")
-    sqlContext.sql("SELECT * FROM points").show(5) //sql
+    sqlContext.sql("SELECT * FROM points").show(5) // sql
 
     println("Finished.")
   }
 }
+
--- engine/examples/src/main/scala/org/apache/spark/examples/sql/shapefile.scala
+++ engine/examples/src/main/scala/org/apache/spark/examples/sql/shapefile.scala
@@ -167,3 +167,13 @@ package object shapefile {
       g.createPolygon(a.head, a.tail.toArray)
   }
 }
+
+object Implicits {
+  class ExtendedByteBuffer(val b: ByteBuffer) extends AnyVal {
+    def little = b.order(ByteOrder.LITTLE_ENDIAN)
+    def big = b.order(ByteOrder.BIG_ENDIAN)
+    def skip(nBytes: Int) = b.position(b.position + nBytes)
+    def skipBbox = b.skip(32) // skip 4 doubles (32 bytes)
+  }
+  implicit def extendByteBuffer(b: ByteBuffer):ExtendedByteBuffer = new ExtendedByteBuffer(b)
+}
