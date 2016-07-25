b63e5df5e232c8ce321a4b57506437b0f32b2f04
Gefei Li <oizz01@sjtu.edu.cn>
2016-07-25 12:21:49 +0800

    rewrite QuadTree

--- engine/sql/core/src/main/scala/org/apache/spark/sql/index/QuadTree.scala
+++ engine/sql/core/src/main/scala/org/apache/spark/sql/index/QuadTree.scala
@@ -17,7 +17,9 @@
 package org.apache.spark.sql.index
 
 import org.apache.spark.sql.spatial.{MBR, Point, Shape}
+
 import scala.collection._
+import scala.util.Random
 
 /**
   * Created by gefei on 16-6-10.
@@ -25,95 +27,81 @@ import scala.collection._
 
 import collection.mutable
 
-class Quadtree[A <% (Int) => Double](xmin: Double, xmax: Double,
-                                   ymin: Double, ymax: Double) {
-  private val MaxObjs = 3
+case class QuadTreeNode(x_low: Double, y_low: Double, x_high: Double, y_high: Double,
+  var children: Array[QuadTreeNode], var objects: Array[(Double, Double, Int)]){
+  private val center_x = (x_low + x_high) / 2
+  private val center_y = (y_low + y_high) / 2
 
-  private class Node(cx: Double, cy: Double, sx: Double, sy: Double,
-                     val objects: mutable.Buffer[A], var children: Array[Node]) {
-    def whichChild(obj: A): Int = {
-      (if (obj(0) > cx) 1 else 0) + (if (obj(1) > cy) 2 else 0)
+  def whichChild(obj: (Double, Double)): Int = {
+    (if (obj._1 > center_x) 1 else 0) + (if (obj._2 > center_y) 2 else 0)
+  }
+  def makeChildren(grouped: Map[Int, Array[(Double, Double, Int)]]): Unit = {
+    children = Array(
+      QuadTreeNode(x_low, y_low, center_x, center_y, null, grouped.getOrElse(0, Array())),
+      QuadTreeNode(center_x, y_low, x_high, center_y, null, grouped.getOrElse(1, Array())),
+      QuadTreeNode(x_low, center_y, center_x, y_high, null, grouped.getOrElse(2, Array())),
+      QuadTreeNode(center_x, center_y, x_high, y_high, null, grouped.getOrElse(3, Array()))
+    )
+  }
+}
+
+case class QuadTree(root: QuadTreeNode) extends Index with Serializable{
+  val MAX_NODES = 3
+  def bulkload(): QuadTreeNode = this.bulkload(root)
+
+  private def bulkload(root: QuadTreeNode): QuadTreeNode = {
+    val grouped = root.objects.groupBy(obj => root.whichChild(obj._1, obj._2))
+    root.makeChildren(grouped)
+    for (child <- root.children) {
+      if (child.objects.length >= MAX_NODES) bulkload(child)
     }
-    def makeChildren() {
-      children = Array(
-        new Node(cx - sx/4, cy - sy/4, sx/2, sy/2, mutable.Buffer(), null),
-        new Node(cx + sx/4, cy - sy/4, sx/2, sy/2, mutable.Buffer(), null),
-        new Node(cx - sx/4, cy + sy/4, sx/2, sy/2, mutable.Buffer(), null),
-        new Node(cx + sx/4, cy + sy/4, sx/2, sy/2, mutable.Buffer(), null)
-      )
+    root.objects = null
+    root
+  }
+
+  def rectQuery(x_min: Double, y_min: Double,
+                x_max: Double, y_max: Double): Array[(Double, Double, Int)] = {
+    val res = new mutable.ArrayBuffer[(Double, Double, Int)]()
+    res ++= searchRecur(root, x_min, y_min, x_max, y_max)
+    res.toArray
+  }
+
+  // interface same with RTree
+  def rectQuery(query: MBR): Array[(Point, Int)] = {
+    val temp_result = this.rectQuery(query.low.coord(0), query.low.coord(1),
+      query.high.coord(0), query.high.coord(1))
+    temp_result.map(item => (Point(Array(item._1, item._2)), item._3))
+  }
+
+  def searchRecur(node: QuadTreeNode, x_min: Double, y_min: Double,
+                  x_max: Double, y_max: Double): mutable.ArrayBuffer[(Double, Double, Int)] = {
+    val res = new mutable.ArrayBuffer[(Double, Double, Int)]()
+    if (node.objects == null) for (child <- node.children) res ++= searchRecur(child,
+      x_min: Double, y_min: Double, x_max: Double, y_max: Double)
+    else {
+      res ++= node.objects.filter(item => item._1 >= x_min
+        && item._1 <= x_max && item._2 >= y_min && item._2 <= y_max)
     }
-    def overlap(obj: A, radius: Double): Boolean = {
-      obj(0) - radius < cx + sx/2 && obj(0) + radius > cx - sx/2 &&
-        obj(1) - radius < cy + sy/2 && obj(1) + radius > cy - sy/2
-    }
-    def overlap(low: A, high: A): Boolean = {
-      val left_upper = new A(low(0), high(1))
-      val right_bottom = new A(high(1), low(0))
-      this.overlap(low, 0) || this.overlap(high, 0) ||
-        this.overlap(left_upper, 0) || this.overlap(right_bottom, 0)
-    }
+    res
+  }
+}
+
+object QuadTree{
+  def apply(entries: Array[(Point, Int)]): QuadTree = {
+    this(entries.map(item => (item._1.coord(0), item._1.coord(1), item._2)))
   }
 
-  private val root = new Node((xmax + xmin)/2, (ymax + ymin)/2, xmax-xmin, ymax-ymin,
-    mutable.Buffer[A](), null)
-
-  def add(obj: A) {
-    addRecur(obj, root)
-  }
-
-  private def addRecur(obj: A, n: Node) {
-    if (n.children == null) {
-      if (n.objects.length < MaxObjs) n.objects += obj
-      else {
-        n.makeChildren()
-        for(o <- n.objects) {
-          addRecur(o, n.children(n.whichChild(o)))
-        }
-        n.objects.clear
-        addRecur(obj, n.children(n.whichChild(obj)))
-      }
-    } else {
-      addRecur(obj, n.children(n.whichChild(obj)))
-    }
-  }
-
-  def searchNeighbors(obj: A, radius: Double): mutable.Buffer[A] = {
-    val ret = mutable.Buffer[A]()
-    searchRecur(obj, radius, root, ret)
-    ret
-  }
-
-  private def searchRecur(obj: A, radius: Double, n: Node, ret: mutable.Buffer[A]) {
-    if (n.children == null) {
-      ret ++= n.objects.filter(o => distance(o, obj) < radius)
-    } else {
-      for (child <- n.children; if (!child.overlap(obj, radius)))
-        searchRecur(obj, radius, child, ret)
-    }
-  }
-
-  def rectSearch(low: A, high: A): mutable.Buffer[A] = {
-    val ret = mutable.Buffer[A]()
-    rectSearchRecur(low, high, root, ret)
-    ret
-  }
-
-  private def rectSearchRecur(low: A, high: A, n: Node, ret: mutable.Buffer[A]): Unit = {
-    if (n.children == null){
-      ret ++= n.objects.filter(o => inRect(o, low, high))
-    } else {
-      for (child <- n.children)
-        if (child.overlap(low, high)) rectSearchRecur(low, high, child, ret)
-    }
-  }
-
-  private def distance(a: A, b: A): Double = {
-    val dx = a(0) - b(0)
-    val dy = a(1) - b(1)
-    math.sqrt(dx * dx + dy * dy)
-  }
-
-  private def inRect(obj: A, low: A, high: A): Boolean = {
-    obj(0) >= low(0) && obj(0) <= high(0) && obj(1) >= low(1) && obj(1) <= high(1)
+  def apply(entries: Array[(Double, Double, Int)]): QuadTree = {
+    // collect the border of the total entries
+    val (x_min, y_min, x_max, y_max) = entries.aggregate(
+      (Double.MaxValue, Double.MaxValue, Double.MinValue, Double.MinValue)
+    )((a: (Double, Double, Double, Double), b: (Double, Double, Int)) =>
+      (math.min(a._1, b._1), math.min(a._2, b._2), math.max(a._3, b._1), math.max(a._4, b._2)),
+      (a: (Double, Double, Double, Double), b: (Double, Double, Double, Double)) =>
+      (math.min(a._1, b._1), math.min(a._2, b._2), math.max(a._3, b._3), math.max(a._4, b._4)))
+    val root = new QuadTreeNode(x_min, y_min, x_max, y_max, null, entries)
+    val quadTree = new QuadTree(root)
+    quadTree.bulkload()
+    quadTree
   }
 }
