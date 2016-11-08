0db9e9e2ed9fcba61766c76ad38c7a21182eed93
Dong Xie <xiedong1993@gmail.com>
2016-11-07 21:09:22 -0700

    Build up dependencies and import packages `spatial` and `index`.

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "simba"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.6.2"
+libraryDependencies += "org.apache.spark" % "spark-catalyst_2.10" % "1.6.2"
+libraryDependencies += "org.apache.spark" % "spark-sql_2.10" % "1.6.2"
+
+libraryDependencies += "com.vividsolutions" % "jts-core" % "1.14.0"
---
+++ src/main/scala/edu/utah/cs/simba/spatial/Shape.scala
@@ -0,0 +1,37 @@
+/*
+ *  Copyright 2016 by Simba Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package edu.utah.cs.simba.spatial
+
+import com.vividsolutions.jts.geom.{Geometry, Polygon => JTSPolygon}
+
+/**
+  * Created by dong on 3/16/16.
+  */
+abstract class Shape extends Serializable {
+  def minDist(other: Shape): Double
+
+  def intersects(other: Shape): Boolean
+
+  def getMBR: MBR
+}
+
+object Shape {
+  final def apply(g: Geometry): Shape = g match {
+    case jtsPolygon : JTSPolygon => new Polygon(jtsPolygon)
+    case _ => null
+  }
+}
